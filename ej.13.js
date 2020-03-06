0d60c4e6f01ea80519137bfac0deb67e6c2b3fcb
Julissa <a314783@uach.mx>
2020-03-05 21:56:59 -0700

    ej.13.js Imprime y cuenta los números que son múltiplos de 2 o de 3 que hay entre 1 y 100.

---
+++ ej.13.js
@@ -0,0 +1,9 @@
+var cont=0;
+    for(var i=1; i<=100;i++){
+		if(i%2==0 || i%3==0){
+			console.log(i);
+			cont=cont+1;
+			}
+		}
+	console.log("Son: " + cont + " multiplos de dos o tres en el intervalo de 1 a 100" );
+
